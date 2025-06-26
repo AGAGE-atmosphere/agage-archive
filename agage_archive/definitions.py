e90e898e385e11020b87b8dfc92b79d466dfb973
Matt Rigby <matt.rigby@bristol.ac.uk>
2025-06-26 14:49:26 +0000

    Adding a generic GCMS instrument

--- agage_archive/definitions.py
+++ agage_archive/definitions.py
@@ -77,7 +77,8 @@ instrument_number = {"UNDEFINED": -1,
                     "LGR": 9,
                     "GCMS-MteCimone": 10,
                     "GCPDD": 11,
-                    "GCMS-Magnum": 12}
+                    "GCMS-Magnum": 12,
+                    "GCMS": 13,}
 
 minimum_averaging_period = {"Picarro": "1H"}
 
