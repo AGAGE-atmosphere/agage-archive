928fbdcc2242aadbc8f7e2ee8ac1d52c912eed7a
joe-pitt <joseph.pitt@bristol.ac.uk>
2025-07-04 14:39:54 +0000

    read release schedule and exclude csvs in all cases

--- agage_archive/io.py
+++ agage_archive/io.py
@@ -302,7 +302,7 @@ def read_nc(network, species, site, instrument,
         ds = read_data_exclude(ds, format_species(species), site, instrument)
 
     # Check against release schedule and remove any data after end date
-    rs = read_release_schedule(network, 
+    rs = read_release_schedule(network,
                             instrument,
                             species=format_species(species),
                             site=site)
@@ -698,7 +698,7 @@ def read_ale_gage(network, species, site, instrument,
         ds = read_data_exclude(ds, format_species(species), site, instrument)
 
     # Check against release schedule
-    rs = read_release_schedule(network, 
+    rs = read_release_schedule(network,
                             instrument,
                             species=format_species(species),
                             site=site)
@@ -850,6 +850,7 @@ def read_gcms_magnum(network, species,
                   site = "MHD",
                   instrument = "GCMS-Magnum",
                   verbose = True,
+                  data_exclude = True,
                   scale = "defaults",
                   baseline = False,
                   resample = False,
@@ -862,6 +863,7 @@ def read_gcms_magnum(network, species,
         site (str, optional): Site. Defaults to "MHD".
         instrument (str, optional): Instrument. Defaults to "GCMS-Magnum".
         verbose (bool, optional): Print verbose output. Defaults to False.
+        data_exclude (bool, optional): Exclude data based on data_exclude.xlsx. Defaults to True.
         scale (str, optional): Calibration scale. Defaults to "defaults".
         baseline (bool, optional): Return baseline dataset. Defaults to False.
         resample (bool, optional): Not used (see run_individual_instrument). Defaults to False.
@@ -962,8 +964,12 @@ def read_gcms_magnum(network, species,
     # Add pollution flag back in temporarily with dimension time
     ds["baseline"] = xr.DataArray(da_baseline.values, dims="time")
 
+    # Remove any excluded data
+    if data_exclude:
+        ds = read_data_exclude(ds, format_species(species), site, instrument)
+
     # Check against release schedule
-    rs = read_release_schedule(network, 
+    rs = read_release_schedule(network,
                             instrument,
                             species=format_species(species),
                             site=site)
@@ -994,6 +1000,7 @@ def read_gcms_magnum(network, species,
 
 def read_gcwerks_flask(network, species, site, instrument,
                        verbose = True,
+                       data_exclude = True,
                        dropna=True,
                        resample = False,
                        scale = "defaults"):
@@ -1005,6 +1012,7 @@ def read_gcwerks_flask(network, species, site, instrument,
         site (str): Site
         instrument (str): Instrument
         verbose (bool, optional): Print verbose output. Defaults to False.
+        data_exclude (bool, optional): Exclude data based on data_exclude.xlsx. Defaults to True.
         dropna (bool, optional): Drop NaN values. Default to True.
         resample (bool, optional): Dummy kwarg, needed for consistency with other functions. Default to False.
         scale (str, optional): Scale to convert to - currently only accepts "defaults", which will read scale_defaults file.
@@ -1104,6 +1112,17 @@ def read_gcwerks_flask(network, species, site, instrument,
     # Set the instrument_type attribute
     # slightly convoluted method, but ensures consistency with combined files
     ds.attrs["instrument_type"] = get_instrument_type(get_instrument_number(instrument))
+
+    # Remove any excluded data
+    if data_exclude:
+        ds = read_data_exclude(ds, format_species(species), site, instrument)
+
+    # Check against release schedule
+    rs = read_release_schedule(network,
+                            instrument,
+                            species=format_species(species),
+                            site=site)
+    ds = ds.sel(time=slice(None, rs))
 
     # Remove all time points where mf is NaN
     if dropna:
